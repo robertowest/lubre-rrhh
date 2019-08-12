2401e000e071762ecd729554fc708d373a69ee54
robertowest <roberto.west@gmail.com>
2019-08-12 09:31:05 -0300

    rrhh

--- apps/rrhh/admin.py
+++ apps/rrhh/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Register your models here.
+from .models import Comunicacion, Domicilio, Empleado, Persona
+
+admin.site.register(Persona)
+admin.site.register(Empleado)
+admin.site.register(Comunicacion)
+admin.site.register(Domicilio)
